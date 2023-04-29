9d70d589e0ba2a39847f3fa775eb60a5e148e49c
max <hanyaomengxi@hotmail.com>
2023-04-29 23:16:00 +0200

    add the real energy production

--- opensolar/chart.py
+++ opensolar/chart.py
@@ -4,13 +4,17 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
+from forecast import get_future_infos
+from algorithms import panel_energy
+from typing import Union
 
 
 @st.cache_data
-def get_kWh_production_dummy(
+def get_kWh_production(
     longitude: float,
     latitude: float,
     date: datetime.date,
+    conversion_efficiency: float = 0.3,
 ) -> float:
     """The ammount of kWh the roof can produce.
 
@@ -20,12 +24,24 @@ def get_kWh_production_dummy(
         latitude (float): The latitude.
         longitude (float): The longitude.
         date (datetime.date): The date of interest.
+        conversion_efficiency: the panel's radiation conversion rate
     """
-    # TODO write the correct function
-    avg_roof_size = 200
-    avg_kWh_per_sqm = 2.92
+    # roofs = get_roof_info(longitude, latitude)
+    roofs = [{"panel_area": 40, "direction": 120}, {"panel_area": 30, "direction": 300}]
+    avg_kwh_per_sqm_dict = get_future_infos(longitude, latitude, date)
 
-    base_kWh_roof = avg_roof_size * avg_kWh_per_sqm
+    base_kWh_roof = 0
+    for roof in roofs:
+        base_kWh_roof += panel_energy(
+            longitude,
+            latitude,
+            date,
+            avg_kwh_per_sqm_dict,
+            roof["panel_area"],
+            roof["direction"],
+            0.35,
+            conversion_efficiency,
+        )
 
     start_date = datetime.date(date.year, 1, 1)
     day_in_year = (date - start_date).days
